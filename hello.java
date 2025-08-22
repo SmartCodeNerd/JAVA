3b124167efae18d40a477c29b91ed3284aa320f2
Goldie2311 <coding.messiah007@gmail.com>
2025-08-22 11:53:56 +0530

    variables

--- hello.java
+++ hello.java
@@ -1,5 +1,17 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(2+3);
+        byte marks = 12;
+        System.out.println(marks);
+        char isLetter = 'A';
+        System.out.println(isLetter);
+        float pi = 3.14f;
+        System.out.println(pi);
+        int marksInt = 150;
+        byte marksByte = marks;
+        System.out.println("Hii");
+        System.out.println(marksInt);
+        System.out.println(marksByte);
     }
-}
+}   
